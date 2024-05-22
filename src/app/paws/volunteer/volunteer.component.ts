0b32b704c2321c5eb86d4bc679e5d82f84866c27
unknown <anandabdul808@gmail.com>
2024-05-22 20:46:58 +0530

    Updated volunteer Page for both user and Admin

--- src/app/paws/volunteer/volunteer.component.ts
+++ src/app/paws/volunteer/volunteer.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { SessionService } from 'src/app/services/session.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-volunteer',
@@ -8,10 +10,39 @@ import { SessionService } from 'src/app/services/session.service';
 })
 export class VolunteerComponent implements OnInit{
 
-  constructor (private SessionService: SessionService) { }
+  constructor (private SessionService: SessionService, private HttpClient:HttpClient) { }
+
+  volunteerForm: any;
 
   ngOnInit(): void {
     this.SessionService.checkUserSession();
+    this.volunteerForm = new FormGroup({
+      name: new FormControl(''),
+      age: new FormControl(''),
+      mobile: new FormControl(''),
+      email: new FormControl(''),
+      address: new FormControl('')
+    })
+  }
+
+  applyVolunteer() {
+    
+    let volunteersAPI = "https://retoolapi.dev/xzR1uz/pawsvolunteerData";
+
+    let volunteerData = {
+      "name": this.volunteerForm.value.name,
+      "age": this.volunteerForm.value.age,
+      "mobile": this.volunteerForm.value.mobile,
+      "email": this.volunteerForm.value.email,
+      "address": this.volunteerForm.value.address
+    };
+
+      this.HttpClient.post(volunteersAPI, volunteerData).subscribe (
+        (data: any) => {
+          alert("Application for Volunteering Submitted Sucessfully!");
+          this.ngOnInit();
+        }
+      );
   }
 
 }
--- src/app/pawsAdmin/volunteer-admin/volunteer-admin.component.ts
+++ src/app/pawsAdmin/volunteer-admin/volunteer-admin.component.ts
@@ -1,10 +1,86 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-volunteer-admin',
   templateUrl: './volunteer-admin.component.html',
   styleUrls: ['./volunteer-admin.component.scss']
 })
-export class VolunteerAdminComponent {
+export class VolunteerAdminComponent implements OnInit{
+
+  constructor (private HttpClient:HttpClient) {}
+
+  ngOnInit(): void {
+    this.getTempVolunteerData();
+    this.getPermVolunteerData();
+  }
+
+  TempVolunteerData: any[] = [];
+  permVolunteerData: any[] = [];
+  name: string = "";
+  age: string = "";
+  mobile: string = "";
+  email: string = "";
+  address: string = "";
+
+  getTempVolunteerData() {
+
+    let volunteersAPI = "https://retoolapi.dev/xzR1uz/pawsvolunteerData";
+    
+    this.HttpClient.get<any>(volunteersAPI).subscribe (
+      (data: any) => {
+        this.TempVolunteerData = data;
+      }
+    );
+  }
+  
+  deleteTempVolunteerDate(data: any) {
+    let volunteersAPI = "https://retoolapi.dev/xzR1uz/pawsvolunteerData" + "/" + data.id;
+    this.HttpClient.delete(volunteersAPI).subscribe (
+      () => {
+        this.ngOnInit();
+      }
+    );
+  }
+
+  savePerVolunteerData(data: any) {
+    
+    let permVolunteersAPI = "https://retoolapi.dev/qzbaSi/pawsPermVolunteers";
+
+    let permVolunteersInfo = {
+      "name": data.name,
+      "age": data.age,
+      "mobile": data.mobile,
+      "email": data.email,
+      "address": data.address,
+      "position": "Newbie Volunteer",
+    }
+    
+    this.HttpClient.post(permVolunteersAPI, permVolunteersInfo).subscribe (
+      () => {
+        alert("We Got a New Volunteer, Congradulations!");
+        this.deleteTempVolunteerDate(data);
+      }
+    );
+  }
+
+  getPermVolunteerData() {
+
+    let permVolunteersAPI = "https://retoolapi.dev/qzbaSi/pawsPermVolunteers";
+    
+    this.HttpClient.get<any>(permVolunteersAPI).subscribe (
+      (data: any) => {
+        this.permVolunteerData = data;
+      }
+    );
+  }
+
+  updatePerVolunteerData() {
+    
+  }
+
+  deletePerVolunteerData() {
+
+  }
 
 }
